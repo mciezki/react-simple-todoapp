c8591693994a2765abec865b24a66554029accbb
Michał Ciężki <m.ciezky97@gmail.com>
2020-10-25 20:53:09 +0100

    Updates

---
+++ precache-manifest.7658bfdbb72818081f8730f64565848e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26ad6aee405d5a59e07d092e376eff2",
+    "url": "/react-simple-todoapp/index.html"
+  },
+  {
+    "revision": "2a0e4dab747b30d4e0ab",
+    "url": "/react-simple-todoapp/static/css/main.e01447e3.chunk.css"
+  },
+  {
+    "revision": "e6c5f9755b0c348d7302",
+    "url": "/react-simple-todoapp/static/js/2.78de81ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-simple-todoapp/static/js/2.78de81ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0e4dab747b30d4e0ab",
+    "url": "/react-simple-todoapp/static/js/main.e0a2088a.chunk.js"
+  },
+  {
+    "revision": "261b04b330c763c8c175",
+    "url": "/react-simple-todoapp/static/js/runtime-main.8cd58083.js"
+  }
+]);
